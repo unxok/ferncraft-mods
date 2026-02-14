6abb6b776b8b17bf31a25af082e11bc9940a065e
unxok <anderluke707@gmail.com>
2026-02-14 11:36:01 -0800

    fixed name collision

--- build-mrpacks.sh
+++ build-mrpacks.sh
@@ -3,16 +3,16 @@ set -euo pipefail
 
 INPUT="modrinth.index.json"
 
-CLIENT_JSON="modrinth.index.json"
-SERVER_JSON="modrinth.index.json"
+CLIENT_JSON_TMP="client-modrinth.index.json"
+SERVER_JSON_TMP="server-modrinth.index.json"
 
 CLIENT_MRPACK="client-mods.mrpack"
 SERVER_MRPACK="server-mods.mrpack"
 
 echo "Creating base JSON files..."
 
-jq '{ dependencies: .dependencies, files: [], formatVersion: .formatVersion, game: .game, name: .name, versionId: .versionId }' "$INPUT" > "$CLIENT_JSON"
-jq '{ dependencies: .dependencies, files: [] }' "$INPUT" > "$SERVER_JSON"
+jq '{ dependencies: .dependencies, files: [], formatVersion: .formatVersion, game: .game, name: .name, versionId: .versionId }' "$INPUT" > "$CLIENT_JSON_TMP"
+jq '{ dependencies: .dependencies, files: [] }' "$INPUT" > "$SERVER_JSON_TMP"
 
 echo "Filtering files..."
 
@@ -21,21 +21,31 @@ jq -c '.files[]' "$INPUT" | while read -r file; do
   server_env=$(echo "$file" | jq -r '.env.server')
 
   if [[ "$client_env" == "required" ]]; then
-    jq ".files += [$file]" "$CLIENT_JSON" > tmp.json && mv tmp.json "$CLIENT_JSON"
+    jq ".files += [$file]" "$CLIENT_JSON_TMP" > tmp.json && mv tmp.json "$CLIENT_JSON_TMP"
   fi
 
   if [[ "$server_env" == "required" ]]; then
-    jq ".files += [$file]" "$SERVER_JSON" > tmp.json && mv tmp.json "$SERVER_JSON"
+    jq ".files += [$file]" "$SERVER_JSON_TMP" > tmp.json && mv tmp.json "$SERVER_JSON_TMP"
   fi
 done
 
 echo "Zipping directly into .mrpack files..."
 
-zip -j "$CLIENT_MRPACK" "$CLIENT_JSON"
-zip -j "$SERVER_MRPACK" "$SERVER_JSON"
+zip -j "$CLIENT_MRPACK" "$CLIENT_JSON_TMP"
+zip -j "$SERVER_MRPACK" "$SERVER_JSON_TMP"
+
+zipnote -w "$CLIENT_MRPACK" <<EOF
+@ $CLIENT_TMP
+@=modrinth.index.json
+EOF
+
+zipnote -w "$SERVER_MRPACK" <<EOF
+@ $SERVER_TMP
+@=modrinth.index.json
+EOF
 
 echo "Cleaning up temp files..."
-rm "$CLIENT_JSON" "$SERVER_JSON"
+rm "$CLIENT_JSON_TMP" "$SERVER_JSON_TMP"
 
 echo "Done! Created:"
 echo " - $CLIENT_MRPACK"
